3b4c325b74101053432d4ea8e456cce031121d02
gabrieldarocha <walter.gdarocha@gmail.com>
2022-06-11 00:58:59 -0300

    Fix lines

---
+++ Ejercicio2/Ejercicio_If.java
@@ -0,0 +1,15 @@
+public class ejercicioIf {
+
+    public static void main(String[] args) {
+        int numerolf;
+        //if
+        if (numerolf < 0){
+            System.out.println("Negativo");
+        } else if (numerolf > 0) {
+            System.out.println("Positivo");
+        } else {
+            System.out.println("Es cero");
+        }
+
+    }
+}
